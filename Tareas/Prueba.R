a44d44b8ef476419f8f5f456443f5ef926cfd486
Urielit12 <uriel.masserdotti@gmail.com>
2026-04-12 17:02:56 -0300

    Tarea 1

--- Tareas/Prueba.R
+++ Tareas/Prueba.R
@@ -1,1 +1,31 @@
-print ("Hello_World")
+library(tidyverse)
+library(nycflights13)
+flights
+glimpse(flights)
+
+#Hola estoy probando
+
+
+
+#Buscamos solo los vuelos cuyo destino es IAH
+flights |>
+  filter(dest == "IAH") |> 
+  group_by(year, month, day) |> 
+  summarize(
+    arr_delay = mean(arr_delay, na.rm = TRUE)
+  )
+
+
+planes |>
+  filter(model == "EMB-145XR") |> 
+  group_by(year, engine, seats)
+  
+
+
+# cargar librerias --------------------------------------------------------
+llibrary (tidyverse)
+
+anac_2025 <- read_csv()
+
+  
+  
